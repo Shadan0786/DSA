7e44a8145fe3fd8c84f345a5c41644497446df3e
Shadan0786 <Shadanashrafi82@gmail.com>
2026-06-30 11:34:53 +0530

    linear search

---
+++ linear search/linearSearch.java
@@ -0,0 +1,16 @@
+public class linearSearch {
+
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,6,7};
+        int target = 4;
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                index = i;
+                break;
+               
+            }
+        }
+        System.out.println("index is : " + index );
+    }
+}
